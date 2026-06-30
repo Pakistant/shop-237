1aad47c177458472ae99f9f879851c2548b378e5
steve pakistant <stdouanla@gmail.com>
2026-06-30 15:43:55 +0100

    feat:ajoute du modale sur les produit

---
+++ src/app/components/modal-product-view/modal-product-view.ts
@@ -0,0 +1,23 @@
+import { DatePipe } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { product } from '../../model/product';
+
+@Component({
+  selector: 'app-modal-product-view',
+  imports: [DatePipe],
+  templateUrl: './modal-product-view.html',
+  styleUrls: ['./modal-product-view.css'],
+})
+export class ModalProductView {
+
+ //INPUT : reçoit le produit à afficher
+product = input<product>();
+// OUTPUT : signal au parent de fermer le modal
+close = output<void>();
+     onCloseClick() {
+     this.close.emit();
+}
+
+
+
+}
